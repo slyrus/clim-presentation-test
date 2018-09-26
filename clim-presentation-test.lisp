9b825f80d08fc5a8fb9e1d1c52a037c248578195
Cyrus Harmon <cyrus@bobobeach.com>
2018-09-26 15:58:29 -0700

    add insert-after and docstrings for insert-before and insert-after

--- clim-presentation-test.lisp
+++ clim-presentation-test.lisp
@@ -83,13 +83,24 @@ of pane."
             (com-move-point old-point (- x px) (- y py))))))))
 
 (defun insert-before (new-item before-item list)
-  "Inserts new-item in list immediately before new-item and returns
-the (destructively) modified list."
+  "If before-item is a member of list, inserts new-item in list
+ immediately before new-item, otherwise new-item is prepended to the
+ beginning of list. Returns the (destructively) modified list."
   (let ((tail (member before-item list)))
             (if tail
                 (progn (rplacd tail (cons (car tail) (cdr tail)))
                        (rplaca tail new-item))
                 (push new-item list)))
+  list)
+
+(defun insert-after (new-item after-item list)
+  "If after-item is a member of list Inserts new-item in list
+immediately after new-item, otherwise it appends new-item to the end
+of list. Returns the (destructively) modified list."
+  (let ((tail (member after-item list)))
+            (if tail
+                (rplacd tail (cons new-item (cdr tail)))
+                (append list (list new-item))))
   list)
 
 (define-clim-presentation-test-command (com-add-point)
